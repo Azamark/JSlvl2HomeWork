7f40e217799260b94172005dad5ef24bb8313399
MGainullin <ya.azamark@yandex.ru>
2022-04-11 21:51:07 +0300

    add cart.js грубое представление о классах для работы с корзиной

---
+++ lesson2/project/js/cart.js
@@ -0,0 +1,75 @@
+'use strict'
+
+class Cart {
+    constructor(elemCartCnt = '.xxxx', elemCartPrice = '.xxxx', elemCartList = '.xxxx', addToCartBtn = '.xxxx') {
+        this._carObjt = { totalCnt: 0, totalPrice: 0, prods: [] };
+        this._cartCnt = document.querySelectorAll(elemCartCnt);
+        this._cartPrice = document.querySelectorAll(elemCartPrice);
+        this._cartList = document.querySelectorAll(elemCartList);
+        this._addToCartBtn = document.querySelectorAll(addToCartBtn);
+    }
+
+    //Слушаем кнопки добавления в корзину. 
+    //ВОПРОС! Правильно описывать обработку события нажатия на кнопку 'добавить в корзину' внутри этого класса?
+    //По идеи обработка кнопок (и вообще интерфейса пользователя) - это низкий уровень абстракции и наверное для этого ужен отдельный класс?
+    _listenAddBtn() {
+        this._addToCartBtn.forEach((btn) => {
+            btn.addEventListener('click', _clickAddToCartBtnHandler);
+        })
+    }
+
+    //Обработка нажатия на кнопку определенного товара
+    _clickAddToCartBtnHandler(event) {
+        //получаем id товара
+        id = event.currentTarget.id;
+        const product = new ProductInCart(id);
+        this._carObjt[prods].push(product.prodInCart)
+    }
+
+    //Расчет количества выбранного товара
+    _calcProductCnt() {
+
+    }
+
+    //Суммарной стоимости выбранного товара
+    _calcProductPrice() {
+
+    }
+
+    _calcCartCnt() {
+
+    }
+
+    _calcCatrSum() {
+
+    }
+
+    //рендер корзины
+    renderCart() {
+        //если товара в корзине нет 
+        .getHTMLString()
+        //иначе обновляем текущие данные
+        ////////////////////////////////
+        //Данные по общим сумме и счетчику
+    }
+}
+
+class ProductInCart {
+    constructor(id) {
+        this.id = id;
+        this.prodInCart = {};
+
+        this._readProductData(id);
+    }
+
+    //Получение данных о выбранном товаре по id
+    _readProductData(id) {
+        //................................
+        // this.prodInCart = {title, price};
+    }
+
+    //Размтка товара в корзине
+    getHTMLString() {
+        return `xxxxxxxxx`
+    }
+}
